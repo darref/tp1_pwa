99f86684dff8c5194610f002014328f243aa7c67
Darref <jordan.ferrad@gmail.com>
2023-12-22 16:03:18 +0100

    meilleur affichage pour les scores en fin de partie

--- Tp_pwa/src/End.tsx
+++ Tp_pwa/src/End.tsx
@@ -2,6 +2,9 @@
 import { Link } from 'react-router-dom';
 
 function End() {
+
+  const scores = JSON.parse(localStorage.getItem("scoresHistory")!) || [];
+
   return (
     <div>
       <h1>Game Over!</h1>
@@ -9,7 +12,13 @@ function End() {
       <Link to="/game">
         <button>Replay</button>
       </Link>
-      <p>Liste des précédents scores: {localStorage.getItem("scoresHistory")}</p>
+      <p>Liste des précédents scores: </p>
+      <ol>
+        {/* Parcourir le tableau des scores et créer un élément <li> pour chaque score */}
+        {scores.map((score: { time: number; }, index:number) => (
+          <li key={index}>{`Score ${index + 1}: ${score.time.toFixed(3)} sec`}</li>
+        ))}
+      </ol>
     </div>
   );
 }
