612704143f202612da2028f9cd6f2c872ba7a40b
Darref <jordan.ferrad@gmail.com>
2023-12-22 11:03:41 +0100

    api geoloc

--- Tp_pwa/src/Game.tsx
+++ Tp_pwa/src/Game.tsx
@@ -21,18 +21,42 @@ function Game() {
   const [counter, setCounter] = useState(0);
   const [position, setPosition] = useState(getRandomPosition());
   const [time, setTime] = useState(0);
+  const [country, setCountry] = useState("");
   const navigate = useNavigate();
+
+  const getCountry = async () => {
+    try {
+      const response = await fetch(
+        'https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=37.42159&longitude=-122.0837&localityLanguage=en'
+      );
+      const data = await response.json();
+      return data.countryName;
+    } catch (error) {
+      console.error('Error fetching country:', error);
+      return '';
+    }
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if(!gamePaused)
+      if (!gamePaused) {
         setTime((prevTime) => {
-          localStorage.setItem("gameTime" , String(prevTime + 0.1));
+          localStorage.setItem('gameTime', String(prevTime + 0.1));
           return prevTime + 0.1;
         });
+      }
     }, 100);
 
     return () => clearInterval(interval);
+  }, [gamePaused]);
+
+  useEffect(() => {
+    const fetchCountry = async () => {
+      const countryName = await getCountry();
+      setCountry(countryName);
+    };
+
+    fetchCountry();
   }, []);
 
   const handleClick = () => {
@@ -63,6 +87,10 @@ function Game() {
       <button style={{ position: 'absolute', bottom: 10, left: 10, color: 'black' }} onClick={togglePauseGame}>
         Game state: {gamePaused? "paused" : "running"}
       </button>
+      
+      <div style={{ position: 'absolute', top: 10, left: 10, color: 'white' }}>
+        Clicked: {country}
+      </div>
     </div>
   );
 }
